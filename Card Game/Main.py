25c33960454ef72f91c024b96af9c229e717c06c
Chromium <62724067+thedevi@users.noreply.github.com>
2020-10-20 14:56:47 +0100

    stfuff

--- Card Game/Main.py
+++ Card Game/Main.py
@@ -5,8 +5,22 @@ fileName = "Credentials.txt"
 credentialJson = {} 
 
 
+def encrypt(text,s):
+  result = ""
+  for i in range(len(text)):
+    char = text[i]
+    if (char.isupper()):
+      result += chr((ord(char) + s-65) % 26 + 65)
+    else:
+      result += chr((ord(char) + s - 97) % 26 + 97)
+  return result
+
+
 def SignIn():
-  print("Sign In")
+  Username = input("What is your Username \n")
+  Password = input("What is your Password \n")
+
+  
   
 
 
@@ -16,37 +30,13 @@ def SignUp():
     newUsername = input("New Username \n")
     newPassword = input("New Password \n")
 
-    credentialJson = {
-        "Username" : newUsername,
-        "Password" : newPassword
-    }
     
-    dump = pickle.dumps(credentialJson)
-
-##
-##    if os.stat(fileName).st_size == 0:
-##      Poo = open(fileName, "w"):
-##      Poo.write(dump)
-##    else:
-##      p = open( fileName,"rb")
-##      data = pickle.load(p)
-##      temp = {}
-##      temp = data
-##      temp.append(dump)
-##      data = temp
-##      pickle.dump(data, l)
-
-
-
-  
-  
-##  
-##
-##
-##if(input("Are you a new user \n") == "Yes"):
-##  SignUp()
-##else:
-##  SignIn()
+    
+    file = open(fileName,'a')
+    file.write(encrypt(newUsername,len(newUsername)) + "|" + encrypt(newPassword,len(newPassword)) + "|" + 0)
+    file.write("\n")
+    file.close()
+    
 
 #Game
 
@@ -70,7 +60,28 @@ def Play(player1Name, player2Name, Cards):
     print("Player 1 Card Colour:", player1CardColour, " | ", "Player 1 Card Num:",player1CardNum)
     print("Player 2 Card Colour:", player2CardColour," | ", "PLayer 2 Card Num:",player2CardNum)
 
-    
+    if(player1CardColour == player2CardColour):
+      print("Card Colours are the same")
+      if(player1CardNum > player2CardNum):
+        print("Player One Wins")
+      else:
+        print("Player Two Wins")
+    else:
+      if(player1CardColour == "red" and player2CardColour == "black"):
+        print("Player One Wins")
+      if(player2CardColour == "red" and player1CardColour == "black"):
+        print("Player Two Wins")
+      if(player1CardColour == "yellow" and player2CardColour == "red"):
+        print("Player One Wins")
+      if(player2CardColour == "yellow" and player1CardColour == "red"):
+        print("Player Two Wins")
+      if(player1CardColour == "black" and player2CardColour == "yellow"):
+        print("Player One Wins")
+      if(player2CardColour == "black" and player1CardColour == "yellow"):
+        print("Player Two Wins")
+
+
+        
 
 
 
@@ -122,5 +133,5 @@ def SignInSystem():
 
 
 
-SignInSystem()
+SignUp()
 
